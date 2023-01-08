f670fdd491f6e822fecd72a0b0fff03744682daf
OmarAbdelghanyyy <105414977+OmarAbdelghanyyy@users.noreply.github.com>
2023-01-08 19:52:30 +0300

    Update EnrolAutomation.py
    updated chromedriver version

--- EnrolAutomation.py
+++ EnrolAutomation.py
@@ -34,19 +34,19 @@ def no_longer_waiting():
     return mat_1720w_status == fra_status == mat_1720a_status
 
 
-s = Service(r'C:\Users\ahmed\Downloads\chromedriver_win32/chromedriver.exe')
+s = Service(r'C:/Users/ahmed/Downloads/chromedriver_win32 (2)/chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 driver.get(
     "https://www.uocampus.uottawa.ca/psp/csprpr9www/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?languageCd=ENG")  # Enrol URL
-
-name_box = driver.find_element(By.NAME, "loginfmt")  # finds login text box
-name_box.send_keys(UNIVERSITY_lOGIN)  # inputs login
+driver.implicitly_wait(20)
+name_box = driver.find_element(By.ID, "i0116")  # finds login text box
+name_box.send_keys("oabde073@uottawa.ca")  # inputs login
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/form[1]/div/div/div[2]/div["
                                                                       "1]/div/div/div/div/div[1]/div["
                                                                       "3]/div/div/div/div["
                                                                       "4]/div/div/div/div/input"))).click()  # First Next Button to proceed to password input page
 password_box = driver.find_element(By.NAME, "passwd")  # finds password text box
-password_box.send_keys(UNIVERSTIY_PASSWORD)  # inputs Password
+password_box.send_keys("Om@1409003")  # inputs Password
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/form[1]/div/div/div[2]/div["
                                                                       "1]/div/div/div/div/div/div[3]/div/div["
@@ -57,7 +57,7 @@ WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/bod
                                                                       "2]/div/div[3]/div[2]/div/div/div["
                                                                       "2]/input"))).click()  # YES button after MFA authorization
 driver.get("https://www.uocampus.uottawa.ca/psc/csprpr9www/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL?Page"
-           "=SSR_SSENRL_LIST&Action=A&ACAD_CAREER=CAR&EMPLID=STUDENTID&ENRL_REQUEST_ID=&INSTITUTION=INST&STRM=TERM")  # Redirects to My Class Schedule Page
+           "=SSR_SSENRL_LIST&Action=A&ACAD_CAREER=CAR&EMPLID=300268565&ENRL_REQUEST_ID=&INSTITUTION=INST&STRM=TERM")  # Redirects to My Class Schedule Page
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/form/div["
                                                                       "5]/table/tbody/tr/td/div/table/tbody/tr[4]/td["
